a5ce11cda618391f6cc326c0d360db108ba4cfe4
ridhi96 <ridhi@uchicago.edu>
2024-03-06 22:03:03 -0600

    updated comments

--- code/data_analysis_llm.py
+++ code/data_analysis_llm.py
@@ -42,18 +42,34 @@ def load_data(uploaded_file):
 # Cached embeddings
 @st.cache_resource
 def get_docembeddings():
+    """
+    This function retrieves FAISS embeddings of books on Statistics stored locally.
+
+    Returns
+        docembeddings : Indexed embeddings 
+    """
     # retrieve pre-saved embeddings
     docembeddings = FAISS.load_local("statistics_llm_faiss_index", OpenAIEmbeddings())
     return docembeddings
     
 
-def getanswer(query, qa_chain):
+def get_answer_from_rag(query, qa_chain):
+    """
+    This function retrieves answer from a Q&A chain for 
+    the question submitted by user.
+
+    Returns
+        output(dict): A dictionary with keys 'Answer' and 'Reference'
+
+    """
+    # perform similiarity search using user question
     relevant_chunks = docembeddings.similarity_search_with_score(query,k=5)
 
     chunk_docs=[]
     for chunk in relevant_chunks:
         chunk_docs.append(chunk[0])
 
+    # invoke Q&A agent and format output
     results = qa_chain.invoke({
         "input_documents": chunk_docs, "question": query
         })
@@ -64,10 +80,17 @@ def getanswer(query, qa_chain):
 
     return output
 
-def processclaim(query, qa_chain):
+def process_q_n_a(query, qa_chain):
+    '''
+    This function handles triggering the Q&A agent and returning the 
+    answer response. 
+
+    Returns
+        output(dict): A dictionary with keys 'Answer' and 'Reference'
+    '''
     try:
         if query:
-            output = getanswer(query, qa_chain)
+            output = get_answer_from_rag(query, qa_chain)
             if not output or 'Answer' not in output or output['Answer'] is None:
                 return {"Answer": "No answer available", "Reference": ""}
         else:
@@ -78,11 +101,18 @@ def processclaim(query, qa_chain):
     return output
 
 def save_conversation_and_display(response, option=None):
+    '''
+    This function saves 'assistant' responses into session state
+    variables for memory purposes.
+    '''
+    # update state 'conversation_memory' and 'conversation_history' 
     st.session_state.conversation_memory.chat_memory.add_ai_message(response)
     st.session_state.conversation_history.append({"role": "assistant", "content": response})
 
+    # write response to web app
     if option == 'Retrieval' or option == 'Web Search':
         st.chat_message("assistant").write(response)
+
 
 ### DuckDuckGo search
 
@@ -92,7 +122,7 @@ tools = [web_search_tool]
 
 
 ### Streamlit Display and Functionality
-    
+
 st.title(":female-technologist: Stassi : A Statistics Assistant :bar_chart:")
 
 # File uploader to get data from user
@@ -120,6 +150,7 @@ if "conversation_history" not in st.session_state:
 for message in st.session_state.conversation_history:
     st.chat_message(message["role"]).write(message["content"])
 
+# Display selectbox for various analysis pathways
 option = st.sidebar.selectbox(
     'Analysis Pathway',
     ('None', 'Data Analysis', 'Retrieval', 'Web Search'))     
@@ -133,66 +164,70 @@ if __name__ == "__main__":
     # User input handling
     if (input := st.chat_input(placeholder="How may I help you?")) and input.strip():
             
-            if not openai_api_key:
-                st.info("Please provide an OpenAI API key to continue.")
-                st.stop()
+        if not openai_api_key:
+            st.info("Please provide an OpenAI API key to continue.")
+            st.stop()
             
-            llm_gpt4 = ChatOpenAI(
-                        temperature=0, model="gpt-4", openai_api_key=openai_api_key, streaming=True
-                    )
+        llm_gpt4 = ChatOpenAI(
+                temperature=0, model="gpt-4", openai_api_key=openai_api_key, streaming=True
+        )
             
-            # Add user message to memory buffer and session state for display
-            st.session_state.conversation_memory.chat_memory.add_user_message(input)
-            st.session_state.conversation_history.append({"role": "user", "content": input})
-            st.chat_message("user").write(input)
+        # Add user message to memory buffer and session state for display
+        st.session_state.conversation_memory.chat_memory.add_user_message(input)
+        st.session_state.conversation_history.append({"role": "user", "content": input})
+        st.chat_message("user").write(input)
             
-            if option == 'Data Analysis':
+        if option == 'Data Analysis':
                 
-                if df is not None:
-                    conversation_context = st.session_state.conversation_memory.load_memory_variables({})
-                   
-                    agent_pandas_df = create_pandas_dataframe_agent(
-                        llm_gpt4,
-                        df,
-                        verbose=True,
-                        agent_type=AgentType.OPENAI_FUNCTIONS,
-                        handle_parsing_errors=True,
-                    )
-
-                    with st.chat_message("assistant"):
-                        stmlt_callback = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
-                        
-                        response = agent_pandas_df.run({"input":conversation_context}, callbacks=[stmlt_callback])
-                        
-                        # Update both the conversation memory and session state for display
-                        save_conversation_and_display(response)
-                else:
-                    st.error("Data frame did not load. Please upload a valid data file.")
-            
-            elif option == 'Retrieval':
-                
-                # Define Q&A chain
-                qa_chain = load_qa_chain(llm_gpt4, chain_type="map_rerank", return_intermediate_steps=True, prompt=RAG_PROMPT)
-                
+            if df is not None:
                 conversation_context = st.session_state.conversation_memory.load_memory_variables({})
-            
-                # Load and cache embedding generation for efficient retrieval
-                docembeddings = get_docembeddings()
+                   
+                agent_pandas_df = create_pandas_dataframe_agent(
+                    llm_gpt4,
+                    df,
+                    verbose=True,
+                    agent_type=AgentType.OPENAI_FUNCTIONS,
+                    handle_parsing_errors=True,
+                )
 
-                # Process query
-                response = processclaim(input, qa_chain)
-
-                # Update both the conversation memory and session state for display
-                save_conversation_and_display(response['Answer'], option)
-            
-            elif option == 'Web Search':
-                web_search_agent = create_react_agent(llm_gpt4, tools, WEB_SEARCH_PROMPT)
-                agent_executor = AgentExecutor(agent=web_search_agent, tools=tools)
-                response = agent_executor.invoke({"input": input})
-             
-                # Update both the conversation memory and session state for display
-                save_conversation_and_display(response['output'], option)
+                with st.chat_message("assistant"):
+                    stmlt_callback = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+                        
+                    response = agent_pandas_df.run({"input":conversation_context}, callbacks=[stmlt_callback])
+                        
+                    # Update both the conversation memory and session state for display
+                    save_conversation_and_display(response)
             else:
-                st.write("Please choose an analysis pathway.")
+                st.error("Data frame did not load. Please upload a valid data file.")
+            
+        elif option == 'Retrieval':
+                
+            # Define Q&A chain
+            qa_chain = load_qa_chain(llm_gpt4, chain_type="map_rerank", return_intermediate_steps=True, prompt=RAG_PROMPT)
+                            
+            # Load and cache embedding generation for efficient retrieval
+            docembeddings = get_docembeddings()
+
+            # Process query
+            response = process_q_n_a(input, qa_chain)
+
+            # Update both the conversation memory and session state for display
+            save_conversation_and_display(response['Answer'], option)
+            
+        elif option == 'Web Search':
+
+            # Define a web search agent using DuckDuckGo search
+            web_search_agent = create_react_agent(llm_gpt4, tools, WEB_SEARCH_PROMPT)
+            agent_executor = AgentExecutor(agent=web_search_agent, tools=tools)
+                
+            # Execute web search to answer question
+            response = agent_executor.invoke({"input": input})
+             
+            # Update both the conversation memory and session state for display
+            save_conversation_and_display(response['output'], option)
+        
+        else:
+            # If no analysis pathway chosen
+            st.write("Please choose an analysis pathway.")
             
     
